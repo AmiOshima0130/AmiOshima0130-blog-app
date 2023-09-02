63ed64b9ef2ab16aa5125cdc49943b39202b0b4b
AMIUSEPC\Ami <amiiiigo.tb@gmail.com>
2023-09-02 20:47:10 +0900

    記事を保存する機能を実装

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,4 +10,19 @@ class ArticlesController < ApplicationController
     def new
       @article = Article.new
     end
+   
+    def create
+      @article = Article.new(article_params)#データを保存する箱
+      @article.save#データを保存
+      if @article.save#もしデータが保存されていたらページに飛ぶ
+         redirect_to article_path(@article)
+      else
+         render :new
+      end
+    end
+    
+    private
+    def article_params
+      params.require(:article).permit(:title, :content)
+    end
   end
