a74a0e666a5c9baee341c1fb02e1c3de543d6492
AMIUSEPC\Ami <amiiiigo.tb@gmail.com>
2023-09-10 08:07:39 +0900

    before_action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,11 @@
 class ArticlesController < ApplicationController
+    before_action :set_article, only: [:show, :edit, :update]
+
     def index
       @articles = Article.all
     end
 
     def show
-      @article = Article.find(params[:id])
     end
 
     def new
@@ -22,11 +23,9 @@ class ArticlesController < ApplicationController
     end
 
     def edit
-      @article = Article.find(params[:id])
     end
   
     def update
-      @article = Article.find(params[:id])
       if @article.update(article_params)
         redirect_to article_path(@article), notice: '更新できました'
       else
@@ -45,5 +44,9 @@ class ArticlesController < ApplicationController
     def article_params#更新するタイトルを付けて○○_paramsとする。paramsのtitleとcontentだけ保存する。
       params.require(:article).permit(:title, :content)
       #articleキーの中のtitleとcontentだけ保存を許す
+    end
+
+    def set_article
+      @article = Article.find(params[:id])
     end
   end
