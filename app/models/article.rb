a99669c0800881ca775a0c41ec322b09047dc864
AMIUSEPC\Ami <amiiiigo.tb@gmail.com>
2023-09-19 15:14:54 +0900

    add custom validation

--- app/models/article.rb
+++ app/models/article.rb
@@ -17,7 +17,15 @@ class Article < ApplicationRecord
     validates :content, length: { minimum: 10 }
     validates :content, uniqueness: true
 
+    validate :validate_title_and_content_length#独自に追加するときはvalidate
+
     def display_created_at
         I18n.l(self.created_at, format: :default)
+    end
+
+    private
+    def validate_title_and_content_length#独自のvalidationはprivateで自分で書く
+        char_count = self.title.length + self.content.length #char_count => character count
+        errors.add(:content, '100文字以上で!') unless char_count > 100
     end
 end
