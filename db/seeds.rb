2d419c3d116a976cc05ad1685e523c5d7cbd6389
AMIUSEPC\Ami <amiiiigo.tb@gmail.com>
2023-09-21 15:54:39 +0900

    seedを修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+# This file should contain all the record creation needed to seed the database with its default values.
 #
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 
-10.times do
-    Article.create(
+#   Character.create(name: 'Luke', movie: movies.first)
+john = User.create!(email: 'john@example.co.jp', password: 'password')
+emily = User.create!(email: 'emily@example.co.jp', password: 'password')
+
+5.times do
+    john.articles.create!(
+    title: Faker::Lorem.sentence(word_count: 5),
+    content: Faker::Lorem.sentence(word_count: 100)
+    )
+end
+
+5.times do
+    emily.articles.create!(
     title: Faker::Lorem.sentence(word_count: 5),
     content: Faker::Lorem.sentence(word_count: 100)
     )
