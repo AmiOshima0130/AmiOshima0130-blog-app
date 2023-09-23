6a7428b325248a4098775b43aa82ac644537a79b
AMIUSEPC\Ami <amiiiigo.tb@gmail.com>
2023-09-23 10:05:56 +0900

    comment機能のcreate action(保存)

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,22 @@
 class CommentsController < ApplicationController
   def new
-    article = Article.find(params[:article_id])
+    article = Article.find(params[:article_id])#articleがあってcommentがある
     @comment = article.comments.build#commentを取得してからcommentをbuildしている。まずはbuildをして空の箱をつくる
+  end
+
+  def create
+    article = Article.find(params[:article_id])
+    @comment = article.comments.build(comment_params)
+    if @comment.save
+        redirect_to article_path(article), notice: 'コメントを追加'
+    else
+        flash.now[:error] = '更新できませんでした'
+        render :new
+    end
+  end
+
+  private
+  def comment_params#paramsはprivateで
+    params.require(:comment).permit(:content)#contentが入力されているcommentだけ許可
   end
 end
