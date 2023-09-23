49cbe27b3a2539e6f0c5fbb84a074f725c1311c0
AMIUSEPC\Ami <amiiiigo.tb@gmail.com>
2023-09-23 15:08:19 +0900

    profileモデルを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,6 +23,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   has_many :articles, dependent: :destroy#userとarticlesを紐づける。userが削除されたら記事も削除される。
+  has_one :profile, dependent: :destroy#profileからみてuserはひとつ
 
   def has_written?(article)
     articles.exists?(id: article.id)
