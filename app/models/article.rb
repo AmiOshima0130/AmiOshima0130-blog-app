b884a59062655edc59d1aaba654db3cb948f9981
AMIUSEPC\Ami <amiiiigo.tb@gmail.com>
2023-09-26 22:11:12 +0900

    いいねの数を表示

--- app/models/article.rb
+++ app/models/article.rb
@@ -36,6 +36,10 @@ class Article < ApplicationRecord
         user.display_name
     end
 
+    def like_count
+      likes.count
+    end
+
     private
     def validate_title_and_content_length#独自のvalidationはprivateで自分で書く
         char_count = self.title.length + self.content.length #char_count => character count
